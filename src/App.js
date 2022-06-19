c9e6844fda155c4ca887cbb19cac9d4fc42eddd8
Bodaghi73 <Bodaghi73>
2022-06-20 03:49:21 +0430

    header component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import {BrowserRouter as Router,Routes, Route} from "react-router-dom";
 
 //pages & components
 import Main from "./pages/main";
+import Header from "./components/header";
 
 function App() {
   return (
     <Router>
+      <Header></Header>
       <Routes>
         <Route path="/" element={<Main/>}/>
       </Routes>
---
+++ src/components/header.js
@@ -0,0 +1,48 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPhoneFlip } from "@fortawesome/free-solid-svg-icons";
+import { faInstagram, faTwitter, faFacebookSquare } from "@fortawesome/free-brands-svg-icons";
+
+//styles
+import "../assets/styles/header.scss"
+function Header() {
+	return (
+		<div className="header py-2">
+			<header className="container">
+				<div className="header-content d-flex justify-content-between align-items-center">
+					<div className="phone-number d-flex align-items-center">
+						<FontAwesomeIcon icon={faPhoneFlip} />
+						<label className="mx-3">تلفن:</label>
+						<a href="tel:+989153085282">09153085282</a>
+					</div>
+					<div className="header-links d-flex align-items-center">
+						<div className="header-menu">
+							<ul className="nav">
+								<li className="nav-item">
+									<a className="nav-link" href="/">
+										میز راهنما
+									</a>
+								</li>
+								<li className="nav-item">
+									<a className="nav-link" href="/">
+										منابع ما
+									</a>
+								</li>
+								<li className="nav-item">
+									<a className="nav-link" href="/">
+										ثبت نام
+									</a>
+								</li>
+							</ul>
+						</div>
+						<div className="social-media">
+							<FontAwesomeIcon icon={faInstagram} />
+							<FontAwesomeIcon icon={faTwitter} className="mx-2" />
+							<FontAwesomeIcon icon={faFacebookSquare} />
+						</div>
+					</div>
+				</div>
+			</header>
+		</div>
+	);
+}
+export default Header;
