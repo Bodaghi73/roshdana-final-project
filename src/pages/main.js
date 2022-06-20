dd6447b07d105ce7d0fe853b4ae6021feda6df0d
Bodaghi73 <Bodaghi73>
2022-06-21 00:57:48 +0430

    article component

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,10 +1,14 @@
 //pages & components
 import Menu from "../components/menu";
+import Article from "../components/article";
 
 function Main() {
-    return(
-        <Menu></Menu>
-    );
+	return (
+		<div>
+			<Menu></Menu>
+			<Article></Article>
+		</div>
+	);
 }
 
 export default Main;
