e3be254403adcec23f93d4de0c87809bbaa04aa2
Bodaghi73 <Bodaghi73>
2022-06-21 04:53:09 +0430

    course coomponent

---
+++ src/components/course.js
@@ -0,0 +1,106 @@
+//style
+import "../assets/styles/course.scss";
+
+//img
+import icon1 from "../assets/imgs/icon1.png";
+import icon2 from "../assets/imgs/icon2.png";
+import icon3 from "../assets/imgs/icon3.png";
+import icon4 from "../assets/imgs/icon4.png";
+import icon5 from "../assets/imgs/icon5.png";
+import icon6 from "../assets/imgs/icon6.png";
+
+function Course() {
+	return (
+		<div className="courses">
+			<div className=" container py-5">
+				<div className="course-title">
+					<h4>آخرین دوره های آیلتس ایز</h4>
+					<span>چرا باید آیلتی ایز را انتخاب کنم؟</span>
+				</div>
+
+				<section className="course row mt-5">
+					<article className="col-md-6 col-lg-4 d-flex align-items-start">
+						<div className="course-icon">
+							<img src={icon1} alt="icon"></img>
+						</div>
+						<div className="course-text me-3">
+							<h5>این همان عنوان مطلب است</h5>
+							<p className="my-3">
+								این یک متن آزمایشی برای تست جانمایی متون است و هیچ کاربرد دیگری ندارد. این یک متن آزمایشی برای تست جانمایی متون است و هیچ کاربرد دیگری
+								ندارد. این یک متن آزمایشی برای تست جانمایی متون است و هیچ کاربرد دیگری ندارد.{" "}
+							</p>
+							<button className="border-0">بیشتر بدانید</button>
+						</div>
+					</article>
+					<article className="col-md-6 col-lg-4 d-flex align-items-start">
+						<div className="course-icon">
+							<img src={icon2} alt="icon"></img>
+						</div>
+						<div className="course-text me-3">
+							<h5>این همان عنوان مطلب است</h5>
+							<p className="my-3">
+								این یک متن آزمایشی برای تست جانمایی متون است و هیچ کاربرد دیگری ندارد. این یک متن آزمایشی برای تست جانمایی متون است و هیچ کاربرد دیگری
+								ندارد. این یک متن آزمایشی برای تست جانمایی متون است و هیچ کاربرد دیگری ندارد.{" "}
+							</p>
+							<button className="border-0">بیشتر بدانید</button>
+						</div>
+					</article>
+					<article className="col-md-6 col-lg-4 d-flex align-items-start">
+						<div className="course-icon">
+							<img src={icon3} alt="icon"></img>
+						</div>
+						<div className="course-text me-3">
+							<h5>این همان عنوان مطلب است</h5>
+							<p className="my-3">
+								این یک متن آزمایشی برای تست جانمایی متون است و هیچ کاربرد دیگری ندارد. این یک متن آزمایشی برای تست جانمایی متون است و هیچ کاربرد دیگری
+								ندارد. این یک متن آزمایشی برای تست جانمایی متون است و هیچ کاربرد دیگری ندارد.{" "}
+							</p>
+							<button className="border-0">بیشتر بدانید</button>
+						</div>
+					</article>
+					<article className="col-md-6 col-lg-4 d-flex align-items-start">
+						<div className="course-icon">
+							<img src={icon4} alt="icon"></img>
+						</div>
+						<div className="course-text me-3">
+							<h5>این همان عنوان مطلب است</h5>
+							<p className="my-3">
+								این یک متن آزمایشی برای تست جانمایی متون است و هیچ کاربرد دیگری ندارد. این یک متن آزمایشی برای تست جانمایی متون است و هیچ کاربرد دیگری
+								ندارد. این یک متن آزمایشی برای تست جانمایی متون است و هیچ کاربرد دیگری ندارد.{" "}
+							</p>
+							<button className="border-0">بیشتر بدانید</button>
+						</div>
+					</article>
+					<article className="col-md-6 col-lg-4 d-flex align-items-start">
+						<div className="course-icon">
+							<img src={icon5} alt="icon"></img>
+						</div>
+						<div className="course-text me-3">
+							<h5>این همان عنوان مطلب است</h5>
+							<p className="my-3">
+								این یک متن آزمایشی برای تست جانمایی متون است و هیچ کاربرد دیگری ندارد. این یک متن آزمایشی برای تست جانمایی متون است و هیچ کاربرد دیگری
+								ندارد. این یک متن آزمایشی برای تست جانمایی متون است و هیچ کاربرد دیگری ندارد.{" "}
+							</p>
+							<button className="border-0">بیشتر بدانید</button>
+						</div>
+					</article>
+					<article className="col-md-6 col-lg-4 d-flex align-items-start">
+						<div className="course-icon">
+							<img src={icon6} alt="icon"></img>
+						</div>
+						<div className="course-text me-3">
+							<h5>این همان عنوان مطلب است</h5>
+							<p className="my-3">
+								این یک متن آزمایشی برای تست جانمایی متون است و هیچ کاربرد دیگری ندارد. این یک متن آزمایشی برای تست جانمایی متون است و هیچ کاربرد دیگری
+								ندارد. این یک متن آزمایشی برای تست جانمایی متون است و هیچ کاربرد دیگری ندارد.{" "}
+							</p>
+							<button className="border-0">بیشتر بدانید</button>
+						</div>
+					</article>
+				</section>
+			</div>
+		</div>
+	);
+}
+
+export default Course;
--- src/pages/main.js
+++ src/pages/main.js
@@ -1,12 +1,14 @@
 //pages & components
 import Menu from "../components/menu";
 import Article from "../components/article";
+import Course from "../components/course";
 
 function Main() {
 	return (
 		<div>
 			<Menu></Menu>
 			<Article></Article>
+			<Course></Course>
 		</div>
 	);
 }
