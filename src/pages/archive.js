2a736169b2baaf44da0ea638a6f3b98e1cf5da60
Bodaghi73 <Bodaghi73>
2022-06-26 17:02:24 +0430

    add component

--- src/pages/archive.js
+++ src/pages/archive.js
@@ -1,12 +1,16 @@
 //pages & components
 import ArchiveTitle from "../components/archive-title";
 import Ielts from "../components/ielts";
+import BuyCourse from "../components/buy-course";
+import Partners from "../components/partners";
 
 function Archive() {
 	return (
 		<div>
 			<ArchiveTitle></ArchiveTitle>
 			<Ielts></Ielts>
+			<BuyCourse></BuyCourse>
+			<Partners></Partners>
 		</div>
 	);
 }
